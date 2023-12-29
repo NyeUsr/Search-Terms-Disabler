1b35986c15557945526826047b6bfebf57109754
NyeUsr <108840615+NyeUsr@users.noreply.github.com>
2023-12-29 14:27:23 -0600

    Add files via upload

---
+++ background.js
@@ -0,0 +1,25 @@
+const braveSearchUrl = "https://search.brave.com/search?q=";
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function (details) {
+    const url = new URL(details.url);
+
+    if (url.hostname === "fakesearch.gov") {
+      const searchQuery = url.searchParams.get("s");
+      const encodedSearchQuery = encodeURIComponent(searchQuery);
+      const redirectUrl = braveSearchUrl + encodedSearchQuery;
+
+      // Add a 5ms delay before redirecting. This makes it so the URL shows instead of the search term(s).
+      setTimeout(() => {
+        chrome.tabs.update(details.tabId, { url: redirectUrl });
+      }, 5);
+
+      return { cancel: true };
+    }
+  },
+  {
+    urls: ["<all_urls>"],
+    types: ["main_frame"]
+  },
+  ["blocking"]
+);
